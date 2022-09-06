c74b9b911de6173622d3c9b3eebad9c09dddbff4
rasengun <69122511+rasengun@users.noreply.github.com>
2022-09-06 13:41:27 +0300

    added modal ingredients

--- src/js/modal.js
+++ src/js/modal.js
@@ -57,3 +57,18 @@
     refs.modal.classList.toggle('is-about-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-ingredients-open]'),
+    closeModalBtn: document.querySelector('[data-ingredients-close]'),
+    modal: document.querySelector('[data-ingredients-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-ingredients-hidden');
+  }
+})();
