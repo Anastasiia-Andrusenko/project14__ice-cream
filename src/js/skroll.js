16a42b4db25a497a398689b1d4d7e0bf2626a575
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2022-09-07 13:32:16 +0300

    add js skroll + fix anchor

---
+++ src/js/skroll.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 300) {
+      $('header').addClass('fixed-header');
+      $('nav div').addClass('visible-title');
+  }
+  else {
+      $('header').removeClass('fixed-header');
+      $('nav div').removeClass('visible-title');
+  }
+});
